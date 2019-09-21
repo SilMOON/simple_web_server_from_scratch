23d4a4cdcccad768ca8fe988fe1343375170ce85
SilMOON <chenminghui8899@gmail.com>
2019-09-21 23:31:28 +0800

    add simple functions

---
+++ target/debug/deps/simple_web_server_from_scratch-7c48e1578639bcb8.d
@@ -0,0 +1,5 @@
+/home/silmoon/MyProjects/Rust/simple_web_server_from_scratch/target/debug/deps/simple_web_server_from_scratch-7c48e1578639bcb8: src/main.rs
+
+/home/silmoon/MyProjects/Rust/simple_web_server_from_scratch/target/debug/deps/simple_web_server_from_scratch-7c48e1578639bcb8.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/simple_web_server_from_scratch.d
@@ -0,0 +1,1 @@
+/home/silmoon/MyProjects/Rust/simple_web_server_from_scratch/target/debug/simple_web_server_from_scratch: /home/silmoon/MyProjects/Rust/simple_web_server_from_scratch/src/main.rs
